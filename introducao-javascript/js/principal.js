25d1b349b35c66fdb234b718cc6e02f2d3cfb6cc
thiago-bento <thiagoribeiro84@gmail.com>
2019-10-22 09:10:38 -0300

    Aula 01 08

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,5 @@
+//Atribui uma variavel a uma seleção de tag
+var titulo = document.querySelector (".container");
+//Seleciona a variavel e altera o conteudo.
+titulo.textContent = "Nutricionista";
+
