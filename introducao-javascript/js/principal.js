943e96a6da802d7ac8e9d5c99ee957b5e4108e3d
Thiago Bento <thiagoribeiro84@gmail.com>
2019-10-23 23:34:14 -0300

    Aula 03 01

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -3,58 +3,46 @@ var titulo = document.querySelector (".container"); //Atribui uma variavel a uma
 
 titulo.textContent = "Nutricionista"; //Seleciona a variavel e altera o conteudo.
 
-/*
-
-function calculaImc () {
-var paciente = document.querySelector ("#primeiro-paciente"); //seleciona o ID da classe toda
-var tdpeso = paciente.querySelector (".info-peso"); //seleciona o peso do ID acima
-var peso = tdpeso.textContent; //seleciona somente o valor do tpeso
-
-var taltura = paciente.querySelector (".info-altura"); // seleciona a altura do ID acima
-var altura = taltura.textContent; //seleciona somente o valor da altura
 
 
-var imc = peso / (altura * altura);
- 
-console.log (imc);
-}
- 
-console.log (calculaImc());
-*/
+var pacientes = document.querySelectorAll (".paciente"); //seleciona o ID da classe toda
 
-
-var paciente = document.querySelector ("#primeiro-paciente"); //seleciona o ID da classe toda
-
-var peso = paciente.querySelector (".info-peso");
-var tdpeso = peso.textContent;
-
-var altura = paciente.querySelector (".info-altura");
-var tdaltura = altura.textContent;
-
-var imc = tdpeso / (tdaltura * tdaltura);
-var tdimc = paciente.querySelector(".info-imc");
-
-var validaAltura = true;
-var validaPeso = true;
-
-
-if (tdpeso > 200 || tdpeso < 20 ) {
-
-    peso.textContent = "Peso incorreto";
-    validaPeso = false;
-} 
-
-if (tdaltura >= 3.00 || tdaltura < 1.00) {
-
-    altura.textContent = "altura incorreta";
-    validaAltura = false;
-}
-
-if (validaAltura && validaPeso) {
-    tdimc.textContent = imc;
+function calculaImc(){
+    var peso = paciente.querySelector (".info-peso");
+    var tdpeso = peso.textContent;
     
-} else {
-    tdimc.textContent = "Altura e/ou peso inválidos!"
+    var altura = paciente.querySelector (".info-altura");
+    var tdaltura = altura.textContent;
+    
+    var imc = tdpeso / (tdaltura * tdaltura);
+    var tdimc = paciente.querySelector(".info-imc");
+    
+    var validaAltura = true;
+    var validaPeso = true;
+    
+    
+        if (tdpeso > 200 || tdpeso < 20 ) {
+    
+            peso.textContent = "Peso incorreto";
+            validaPeso = false;
+        } 
+    
+        if (tdaltura >= 3.00 || tdaltura < 1.00) {
+    
+            altura.textContent = "altura incorreta";
+            validaAltura = false;
+        }
+    
+        if (validaAltura && validaPeso) {
+            tdimc.textContent = imc.toFixed(2   );
+            
+            } else {
+            tdimc.textContent = "Altura e/ou peso inválidos!"
+        }
+    }
+
+for (var i=0 ; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    calculaImc();
 }
-    console.log (tdaltura);
-    console.log (altura);
+   
