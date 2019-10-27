b49a31694976716dd90a5dd87749800001cc8ff0
Thiago Bento <thiagoribeiro84@gmail.com>
2019-10-27 19:35:38 -0300

    Final Aula 4

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -47,43 +47,47 @@ for (var i=0 ; i < pacientes.length ; i++){
     calculaImc();
 }
    
-titulo.addEventListener("click", function (){
+titulo.addEventListener("click", function (){ //quando clicar no titulo exibira a msg.
     console.log ("Titulo da página");
 }); 
 
+var botaoAdicionar = document.querySelector ("#adicionar-paciente");  // cria uma variavel para o ID do botao, para fazer algo.
 
+botaoAdicionar.addEventListener("click", function(event){ //atribui a variavel do botáo a funcao adicionar uma lista de eventos e cria uma funcao anonima dentro qnd clicar.
 
-var botaoAdicionar = document.querySelector ("#adicionar-paciente")
+        event.preventDefault(); //não deixa a tela atualizar quando clicar no botao.
 
-botaoAdicionar.addEventListener("click", function(event){
-        event.preventDefault(); //não deixa a tela atualizar.
-        
-        var botaoForm = document.querySelector("#btAdicionar")
+        var botaoForm = document.querySelector("#btAdicionar");  //cria uma variavel para o ID do <Form> para poder ler as informacoes que ha dentro dele.
 
-        var inNome = botaoForm.nome.value;
+        var inNome = botaoForm.nome.value; //atribiu o valor digitado à varivel do nome do id dentro do form.
         var inpeso = botaoForm.peso.value;
         var inAltura = botaoForm.altura.value;
         var inGordura = botaoForm.gordura.value;
         
-        console.log (inNome, inpeso, inAltura, inGordura);
+        var trNova = document.createElement("tr");  //atribiu uma varivel a uma nova <TR>
 
-        var trNova = document.createElement("tr");
-
-        var tdNome = document.createElement("td");
+        var tdNome = document.createElement("td"); //atribiu uma varivel a uma nova <TD>
         var tdPeso = document.createElement("td");
         var tdAltura = document.createElement("td");
         var tdGordura = document.createElement("td");
-        var tdImc = document.createElement("td");
+        var tdImc= document.createElement("td");
 
-   
+        tdNome.textContent = inNome; // com a variavel para criar uma TD, pega o valor da varivel do input.
+        tdPeso.textContent = inpeso;
+        tdAltura.textContent = inAltura;
+        tdGordura.textContent = inGordura;
+         
+       trNova.appendChild(tdNome); //Cria o valor da TD dentro da TR
+       trNova.appendChild(tdPeso);
+       trNova.appendChild(tdAltura); 
+       trNova.appendChild(tdGordura);
        
-       6 - ler o valor que foi pego nas variaveis TD e TR -> nomedavariavel.textContent = nome do TD
+       var tbodyNovo = document.querySelector ("#tabela-pacientes");
+
+       tbodyNovo.appendChild(trNova);
+
        
-       7 - Atribuir os valores da TR nas TD (filho) - > nomedavaeriaveltr.appendChild(nomedavariavelTD)
-       
-       8- criar uma variavel para incluir a TR dentdo do <tbory> (tem im id de tabela-paciente) -> nomedavariavel = document.queruSelector (# nome do id do Tbody) .
-       
-       9- criando os filhos do Tbody - > nomedavariaveldoIDtbory.appendChild(nomevariavelTR).
+});
 
 
 
