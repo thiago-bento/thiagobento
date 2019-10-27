a7cc4694e6ac2e2aee914950b36d31366b70538a
Thiago Bento <thiagoribeiro84@gmail.com>
2019-10-27 19:56:06 -0300

    Aula 05 01

--- introducao-javascript/js/adicionaPaciente.js
+++ introducao-javascript/js/adicionaPaciente.js
@@ -1,56 +1,4 @@
 
-var titulo = document.querySelector (".container"); //Atribui uma variavel a uma seleção de tag
-
-titulo.textContent = "Nutricionista"; //Seleciona a variavel e altera o conteudo.
-
-var pacientes = document.querySelectorAll (".paciente"); //seleciona o ID da classe toda
-
-function calculaImc(){
-    var peso = paciente.querySelector (".info-peso");
-    var tdpeso = peso.textContent;
-    
-    var altura = paciente.querySelector (".info-altura");
-    var tdaltura = altura.textContent;
-    
-    var imc = tdpeso / (tdaltura * tdaltura);
-    var tdimc = paciente.querySelector(".info-imc");
-    
-    var validaAltura = true;
-    var validaPeso = true;
-    
-    
-        if (tdpeso > 200 || tdpeso < 20 ) {
-    
-            peso.textContent = "Peso incorreto";
-            paciente.style.backgroundColor = "lightcoral";
-            
-            validaPeso = false;
-        } 
-    
-        if (tdaltura >= 3.00 || tdaltura < 1.00) {
-    
-            altura.textContent = "altura incorreta"
-            paciente.classList.add ("paciente-invalido");
-            validaAltura = false;
-        }
-    
-        if (validaAltura && validaPeso) {
-            tdimc.textContent = imc.toFixed(2);
-            
-            } else {
-            tdimc.textContent = "Altura e/ou peso inválidos!"
-        }
-    }
-
-for (var i=0 ; i < pacientes.length ; i++){
-    var paciente = pacientes[i];
-    calculaImc();
-}
-   
-titulo.addEventListener("click", function (){ //quando clicar no titulo exibira a msg.
-    console.log ("Titulo da página");
-}); 
-
 var botaoAdicionar = document.querySelector ("#adicionar-paciente");  // cria uma variavel para o ID do botao, para fazer algo.
 
 botaoAdicionar.addEventListener("click", function(event){ //atribui a variavel do botáo a funcao adicionar uma lista de eventos e cria uma funcao anonima dentro qnd clicar.
@@ -88,6 +36,3 @@ botaoAdicionar.addEventListener("click", function(event){ //atribui a variavel d
 
        
 });
-
-
-
---
+++ introducao-javascript/js/calculaImc.js
@@ -0,0 +1,46 @@
+
+
+var pacientes = document.querySelectorAll (".paciente"); //seleciona o ID da classe toda
+
+for (var i=0 ; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector (".info-peso");
+    var tdpeso = peso.textContent;
+    
+    var altura = paciente.querySelector (".info-altura");
+    var tdaltura = altura.textContent;
+    
+    var imc = tdpeso / (tdaltura * tdaltura);
+    var tdimc = paciente.querySelector(".info-imc");
+    
+    var validaAltura = true;
+    var validaPeso = true;    
+    
+        if (tdpeso > 200 || tdpeso < 20 ) {
+    
+            peso.textContent = "Peso incorreto";
+            paciente.style.backgroundColor = "lightcoral";
+            
+            validaPeso = false;
+        } 
+    
+        if (tdaltura >= 3.00 || tdaltura < 1.00) {
+    
+            altura.textContent = "altura incorreta"
+            paciente.classList.add ("paciente-invalido");
+            validaAltura = false;
+        }
+    
+        if (validaAltura && validaPeso) {
+            tdimc.textContent = imc.toFixed(2);
+            
+            } else {
+            tdimc.textContent = "Altura e/ou peso inválidos!"
+        }
+}
+   
+
+
+
+
