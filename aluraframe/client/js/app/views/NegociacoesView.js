3268a72953cc3bd0d2f963cac26f02440bd4b1c5
thiago-bent <thiagobento@nasajon.com.br>
2020-03-17 20:24:44 -0300

    final aula 5

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -6,7 +6,7 @@ class NegociacoesView {
         this._elemento = elemento;
     }
 
-        _template() {
+        _template(model) {
 
             return `
             <table class="table table-hover table-bordered">
@@ -19,14 +19,34 @@ class NegociacoesView {
                             </tr>
                         </thead>
                         <tbody>
+                        ${model.negociacoes.map(n => 
+                            `   <tr>
+                                    <td>${DateHelper.dataParaTexto(n.data)}</td>
+                                    <td>${n.quantidade}</td>
+                                    <td>${n.valor}</td>
+                                    <td>${n.volume}</td>
+                                </tr>
+                            `).join('')}
                         </tbody>
+
+                        <tfoot>
+
+                            <td colspan="3"> </td>
+
+                            <td>
+                            ${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                        </td>
+                            
+                        </tfoot>
+
+
                  </table>
                 `;
               }
     
-        update() {
+              update(model) {
 
-            this._elemento.innerHTML = this._template();
+                this._elemento.innerHTML = this._template(model);
         }
 
   
