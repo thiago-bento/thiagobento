d4355abfd1c5c7a739a828bd7a49ab72d8f3a960
thiago-bent <thiagobento@nasajon.com.br>
2020-04-07 11:37:38 -0300

    PHP

---
+++ PHP/ola-mundo.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+$idade = 17;
+$nome = 'thiago';
+
+if ($idade >=18){
+    echo "Você tem $idade, $nome pode entrar ". "\n";
+}else{
+    echo 'Vpce é menor, nao pode entrar'. PHP_EOL;
+}
